3a3020094dca2706c97d638aaacf1ec9f56d1507
Carlos Argudo <cfargudo@sudamericano.edu.ec>
2024-06-13 11:15:53 -0500

    strings

---
+++ strings.js
@@ -0,0 +1,8 @@
+"use strict";
+
+'use strict';
+let estudiantes = ["Carlos","Fernando","Paula","Ernesto","Mateo",]
+
+for (let i=0;1<8;i++){
+        alert("los estudiantes del curos son: " + estudiantes[i])
+}
